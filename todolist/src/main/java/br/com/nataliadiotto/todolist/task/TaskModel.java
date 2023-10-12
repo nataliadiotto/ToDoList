73b481aa882a74e8cff4e2ef9f94959cfd790e57
Natália Diotto <diottonatalia@gmail.com>
2023-10-12 10:30:13 -0300

    Creating TaskModel class

---
+++ todolist/src/main/java/br/com/nataliadiotto/todolist/task/TaskModel.java
@@ -0,0 +1,36 @@
+package br.com.nataliadiotto.todolist.task;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID") //automatically generate Id
+    private UUID id;
+
+    private String description;
+
+    @Column(length = 50) //limiting title length
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    private UUID userId;
+
+}
+
+
