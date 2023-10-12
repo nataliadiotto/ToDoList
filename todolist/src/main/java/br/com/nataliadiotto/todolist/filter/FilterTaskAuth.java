d74f19da4967114e5ee51e9c2580d31b9b61fc8c
Natália Diotto <diottonatalia@gmail.com>
2023-10-12 14:28:51 -0300

    Adding authorization validation

--- todolist/src/main/java/br/com/nataliadiotto/todolist/filter/FilterTaskAuth.java
+++ todolist/src/main/java/br/com/nataliadiotto/todolist/filter/FilterTaskAuth.java
@@ -1,20 +1,35 @@
 package br.com.nataliadiotto.todolist.filter;
 
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component //asking spring to manage this class
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
 
-        chain.doFilter(request, response);
+        //catch authentication (user and password)
+        var authorization = request.getHeader("Authorization");
+        var user_password = authorization.substring("Basic".length()).trim();
+
+        System.out.println("Authorization");
+        System.out.println(user_password);
+        
+        //validate user
+        
+        //validate password
+        
+        
+
+        filterChain.doFilter(request, response);
     }
-
-
 }
