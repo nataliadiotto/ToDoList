80a63e75f5fa95f11cf983b58f4a68c8ba5aff06
Natália Diotto <diottonatalia@gmail.com>
2023-10-12 19:24:57 -0300

    Listing User's tasks

--- todolist/src/main/java/br/com/nataliadiotto/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/nataliadiotto/todolist/task/TaskController.java
@@ -7,12 +7,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -35,12 +33,19 @@ public class TaskController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Start and end date must be after current date.");
         }
 
-        if (taskModel.getCreatedAt().isAfter(taskModel.getCreatedAt())) {
+        /*if (taskModel.getCreatedAt().isAfter(taskModel.getEndAt())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("End date must be after creation date.");
-        }
+        }*/
 
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/") //return everything related to that user
+    public List<TaskModel> list(HttpServletRequest request) {
+        var userId = request.getAttribute("userId");
+        var tasks = this.taskRepository.findByUserId((UUID) userId);
+        return tasks;
     }
 
 }
--- todolist/src/main/java/br/com/nataliadiotto/todolist/task/TaskRepository.java
+++ todolist/src/main/java/br/com/nataliadiotto/todolist/task/TaskRepository.java
@@ -2,8 +2,10 @@ package br.com.nataliadiotto.todolist.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
 
+    List<TaskModel> findByUserId(UUID userId);
 }
